77fb938ee9d948f5f4002d8a72df91330509e74b
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-06-09 20:42:29 -0500

    initial project design and structure

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsComponent } from './components/gifs/gifs.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }
