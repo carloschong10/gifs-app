e378b59004641dd4f19776504ca9a2a7ab147164
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-06-15 17:07:33 -0500

    GifsService and Validations

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-search-box',
@@ -18,12 +19,16 @@ export class SearchBoxComponent {
   @ViewChild('txtTagInput') //hace referencia a una referencia local del html
   public tagInput!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService: GifsService) { }
 
   // searchTag(newTag: string) { //ya no es necesario enviarlo por parametro ya que lo estamos enlazando a través del ViewChield tagInput
   searchTag() {
     const newTag = this.tagInput.nativeElement.value;
-    console.log({newTag});
+    // console.log({newTag});
+
+    this.gifsService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
   }
 
 }
---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class GifsService {
+
+    private _tagsHistory: string[] = [];
+
+    constructor() { }
+
+    get tagsHistory() {
+        return [...this._tagsHistory];
+    }
+
+    private organizeHistory(tag: string) {
+        tag = tag.toLowerCase().trim();
+        this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+        this._tagsHistory.unshift(tag);
+        this._tagsHistory = this._tagsHistory.splice(0, 10);
+    }
+
+    searchTag(tag: string): void {
+        if (tag.length === 0) return;
+
+        this.organizeHistory(tag);
+        // this._tagsHistory.unshift(tag);
+        // console.log(this._tagsHistory)
+    }
+
+}
