4a2e404be4d5d5ad6479b74f3ba9d6689f098f73
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-06-15 18:59:03 -0500

    Add a data type to an HTTP response

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
     private _tagsHistory: string[] = [];
+    private apiKey: string = 'mBsRuvAcC5VKTaxHPveTp09PfU3pBrE5';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+    public gifList: Gif[] = [];
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
     get tagsHistory() {
         return [...this._tagsHistory];
@@ -18,12 +23,31 @@ export class GifsService {
         this._tagsHistory = this._tagsHistory.splice(0, 10);
     }
 
+    // async searchTag(tag: string): Promise<void> {
     searchTag(tag: string): void {
         if (tag.length === 0) return;
-
         this.organizeHistory(tag);
-        // this._tagsHistory.unshift(tag);
-        // console.log(this._tagsHistory)
+
+        // esta seria una peticion comun con javascript a traves del api fetch
+        /*
+        const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=mBsRuvAcC5VKTaxHPveTp09PfU3pBrE5&q=valorant&limit=10');
+        const data = await resp.json();
+        console.log(data);
+        */
+
+        //pero en angular se usa mejor los Observables
+        const params = new HttpParams()
+            .set('api_key', this.apiKey)
+            .set('limit', '10')
+            .set('q', tag);
+
+        this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+            .subscribe(resp => {
+                this.gifList = resp.data;
+                console.log({gifs: this.gifList})
+            }
+
+            )
     }
 
 }
