743b3dea0aecf061ec48bd65ae6865c1861528c9
Carlos Alfonso Chong Zapata <112518890+carloschong10@users.noreply.github.com>
2024-06-16 02:21:26 -0500

    Search by button in sideBar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -9,7 +9,11 @@ import { GifsService } from 'src/app/gifs/services/gifs.service';
 export class SidebarComponent {
   constructor(private gifsService: GifsService) { }
 
-  get tags() {
+  get tags(): string[] {
     return this.gifsService.tagsHistory;
+  }
+
+  searchTagSideBar(tag: string): void {
+    this.gifsService.searchTag(tag);
   }
 }
